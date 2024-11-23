299b2bf83b67c6c485d122f8afb41c39e8e1c4a1
Lukas Gabrys <gabryl1@mcmaster.ca>
2024-11-23 10:10:45 -0500

    Coomit Debugs

--- Sorter/obj/Debug/net9.0/Sorter.AssemblyInfo.cs
+++ Sorter/obj/Debug/net9.0/Sorter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sorter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29e4f12ab0ec3645074d012bd430fa3b7de9b62e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7205ddad2b58e4881fb866890dd4baa985504341")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sorter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sorter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
